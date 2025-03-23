a18c375bda56e327bf520892e7b8aabb7479c0cb
mmaster <myles1551@gmail.com>
2025-03-22 22:01:46 -0500

    workign on esp32 stuff

--- src/ui/overlay.cpp
+++ src/ui/overlay.cpp
@@ -4,5 +4,7 @@
 ------------------------------INITIALIZATION-----------------------------------
 -----------------------------------------------------------------------------*/
 void Overlay::init(VkDevice device, VkPhysicalDevice physicalDevice, VkRenderPass renderpass) {
-
+	device_ = device;
+	physicalDevice_ = physicalDevice;
+	renderpass_ = renderpass;
 }
