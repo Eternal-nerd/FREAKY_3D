0fcebe53201ee00d9260f48f28189ffd6a1ff004
Myles Master <myles1551@gmail.com>
2025-04-27 22:58:44 -0500

    pitiful, grind on button tomorrow

--- src/ui/button.cpp
+++ src/ui/button.cpp
@@ -4,6 +4,24 @@
 ------------------------------INITIALIZATION-----------------------------------
 -----------------------------------------------------------------------------*/
 void Button::init(OverlayState& state, OverlayElementState* elementState, const std::string& id, const std::string label, glm::vec2 position, glm::vec2 sizePixels, glm::vec2 fontSize, int fontIndex, int backgroundIndex) {
+    util::log(name_, "initializing overlay button");
+    
+    state_ = &state;
+    elementState_ = elementState;
+    
+    if (elementState_ == nullptr) {
+		unique_ = true;
+		elementState_ = new OverlayElementState;
+		elementState_->dragged = false;
+		elementState_->hovered = false;
+		elementState_->interaction = 0;
+		elementState_->movable = true; // FIXME
+
+	}
+	else {
+		unique_ = false;
+	}
+
 
 }
 
@@ -42,5 +60,9 @@ void Button::resetInteraction() {
 ------------------------------CLEANUP------------------------------------------
 -----------------------------------------------------------------------------*/
 void Button::cleanup() {
+    util::log(name_, "cleaning up Button resources");
 
+	if (unique_) {
+		delete elementState_;
+	}
 }
