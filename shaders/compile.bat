e6f3defa187107eaef5dbf12cc98fd747a6e91d8
mmaster <myles1551@gmail.com>
2025-07-09 21:14:04 -0500

    compile script fix

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,4 +1,4 @@
-start "glslc" "C:/VulkanSDK/1.3.296.0/Bin/glslc.exe" main.vert -o compiled/main_vert.spv
-start "glslc" "C:/VulkanSDK/1.3.296.0/Bin/glslc.exe" main.frag -o compiled/main_frag.spv
-start "glslc" "C:/VulkanSDK/1.3.296.0/Bin/glslc.exe" overlay.vert -o compiled/overlay_vert.spv
-start "glslc" "C:/VulkanSDK/1.3.296.0/Bin/glslc.exe" overlay.frag -o compiled/overlay_frag.spv
+glslc main.vert -o compiled/main_vert.spv
+glslc main.frag -o compiled/main_frag.spv
+glslc overlay.vert -o compiled/overlay_vert.spv
+glslc overlay.frag -o compiled/overlay_frag.spv
